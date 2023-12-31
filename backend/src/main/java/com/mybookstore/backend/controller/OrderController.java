ccc80b7292c0d52d86f33652abd28d5878c63b6a
Doguhan <doguhan0erol@gmail.com>
2023-12-31 14:50:25 +0300

    OrderController is fixed.

--- backend/src/main/java/com/mybookstore/backend/controller/OrderController.java
+++ backend/src/main/java/com/mybookstore/backend/controller/OrderController.java
@@ -3,10 +3,7 @@ package com.mybookstore.backend.controller;
 import com.mybookstore.backend.model.Order;
 import com.mybookstore.backend.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/order")
@@ -17,7 +14,7 @@ public class OrderController {
     public OrderController(OrderService orderService){
         this.orderService = orderService;
     }
-    @GetMapping("/order")
+    @PostMapping("/order")
     public Order createOrder(@RequestBody Order order){
         return orderService.createOrder(order);
     }
