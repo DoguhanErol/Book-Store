ba13c2f0421f4c90472d21f7129c5aa175875e19
Doguhan <doguhan0erol@gmail.com>
2023-12-31 15:16:08 +0300

    Added POST method for add a book.

--- backend/src/main/java/com/mybookstore/backend/controller/BookController.java
+++ backend/src/main/java/com/mybookstore/backend/controller/BookController.java
@@ -3,10 +3,7 @@ package com.mybookstore.backend.controller;
 import com.mybookstore.backend.model.Book;
 import com.mybookstore.backend.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +25,9 @@ public class BookController {
     @GetMapping("/{category}")
     public List<Book> getBooksByCategory(@PathVariable String category){
         return bookService.getBooksByCategory(category);
+    }
+    @PostMapping
+    public Book createBook(@RequestBody Book book){
+        return bookService.saveBook(book);
     }
 }
