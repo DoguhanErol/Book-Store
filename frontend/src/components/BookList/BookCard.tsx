fa9793bbb3c409112ff5679d3cf54dae786b2b2f
Doguhan <DoguhanErolJob@gmail.com>
2024-04-27 17:56:36 +0300

    U

--- frontend/src/components/BookList/BookCard.tsx
+++ frontend/src/components/BookList/BookCard.tsx
@@ -3,15 +3,15 @@ const BookCard:React.FC = () => {
   return (
     <>
     <article className="w-[35rem] m-2">
-        <div className="card card-side bg-opacity-95 bg-accent shadow-lg w-[35rem]">
-          <figure className="w-40"><img src={bookImg} alt="Book"/></figure>
-          <div className="card-body">
-          <div className="badge badge-secondary">Philosophy</div>
+        <div className="card card-side bg-opacity-95 bg-accent shadow-lg sm:w-[35rem]">
+          <figure className="w-36 sm:w-40"><img src={bookImg} alt="Book"/></figure>
+          <div className="card-body bg-none sm:bg-secondary sm:bg-opacity-40 rounded-lg">
+          <div className="badge badge-secondary rounded-lg">Philosophy</div>
             <h3 className="card-title text-neutral-800">Böyle Söyledi Zerdüşt</h3>
             <h4 className=" text-neutral-600">Frederich Nietche</h4>
-            <div className="divider divider-secondary text-2xl">30$</div>
+            <div className="divider divider-info text-lg sm:text-2xl">30$</div>
             <div className="card-actions  justify-end">
-                <button className="btn btn-primary">Buy</button>
+                <button className="btn btn-primary rounded-full sm:text-lg">Buy</button>
             </div>
           </div>
         </div>
--- frontend/src/components/Shared/Header.tsx
+++ frontend/src/components/Shared/Header.tsx
@@ -5,12 +5,12 @@ import leftArrowImg from '../../assets/leftArrow.png'
 const Header:React.FC = () => {
   return (
     <header>
-        <nav className="navbar bg-neutral rounded-md shadow-md bg-opacity-95 p-3">
-            <div className="flex-none mx-1 md:mx-3">
+        <nav className="navbar z-50 bg-neutral rounded-md shadow-md bg-opacity-95 p-3">
+            <div className="flex-none  md:mx-3">
             <label htmlFor="my-drawer" className="btn btn-outline btn-accent ">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
             </label>
-              
+
             </div>
             <div className="flex-1">
               <a href="/" className="btn btn-outline btn-accent  shadow-none ">
